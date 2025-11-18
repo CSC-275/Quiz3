7143ecc1439223c531f8bffcdb8a6ab6813dfa88
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-11-18 12:56:55 -0500

    Initial Commit

--- Game.h
+++ Game.h
@@ -3,18 +3,11 @@
 #include <vector>
 #include "KenoNumbers.h"
 #include "Player.h"
-
+// complete the Game class declaration
 class Game {
-private:
-    KenoNumbers m_keno;
-    Player m_player;
-    std::vector<int> m_matches;
-public:
-    Game();
-    void printMatches(std::vector<int>playerPicks,std::vector<int>kenoNumbers);
-    void displayResults();
-    void playGame();
+
 };
 
 #endif //GAME_H
+
 
